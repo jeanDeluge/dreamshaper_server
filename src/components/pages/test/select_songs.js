f9edb63b8c1297664640625b6b83897336bd3e45
jeandelgue <jeandelugedev@gmail.com>
2023-09-24 17:31:40 +0900

    chore: class to className

--- src/components/pages/test/select_songs.js
+++ src/components/pages/test/select_songs.js
@@ -54,11 +54,11 @@ function Main() {
       <div className='relative'>
         <div className={uploading === true ? 'opacity-20' :"" }>
         <img src={artist_list[id - 1].image} width={"300px"} className='m-auto' />
-        <ul class="max-w-screen w-5/6 m-10 m-auto my-10 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+        <ul className="max-w-screen w-5/6 m-10 m-auto my-10 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
 
             {song_list.map(e => {
             return (
-                <li class="w-full flex justify-between px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                <li className="w-full flex justify-between px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">
                 <div className='font-semibold test-lg m-3'>
                     {e.name}
                 </div>
@@ -70,14 +70,14 @@ function Main() {
                 </li>
             )
             })}
-            <li class="w-full flex justify-between px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+            <li className="w-full flex justify-between px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">
             <div className='font-semibold test-lg m-3'>
                 새로 음성 입히기
             </div>
             <div>
-                <input onChange={handleFileChange} class="block w-96 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" accept='audio/*' />
+                <input onChange={handleFileChange} className="block w-96 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" accept='audio/*' />
             </div>
-            <button type="button" onClick={sendSong} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            <button type="button" onClick={sendSong} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </li>
         </ul>
         </div>
